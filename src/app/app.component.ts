9b808b8fe546db9f59b39087c95fa63c03ee94ae
Nina Nguyen <ninalp.nguyen@gmail.com>
2018-07-05 16:12:16 +0200

    added google fonts, fixed content of contactpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import {ActivityPage} from "../pages/activity/activity";
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = ContactPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
