115bdf2daa3fbd034009dd4350e4367688a36ee1
Millagro <emilie.tije@web.de>
2018-07-09 21:48:30 +0200

    removed unused imports

--- src/pages/impressum/impressum.ts
+++ src/pages/impressum/impressum.ts
@@ -6,7 +6,6 @@ import {ScanPage} from "../scan/scan";
 import {ContactPage} from "../contact/contact";
 import { Storage } from '@ionic/storage';
 import {LoginPage} from "../login/login";
-import {RestProvider} from "../../providers/rest/rest";
 
 
 
@@ -28,8 +27,7 @@ export class ImpressumPage {
               private storage: Storage,
               public alertCtrl: AlertController,
               private toastCtrl: ToastController,
-              public app: App,
-              public restProvider: RestProvider) {
+              public app: App) {
   }
 
     goToProfile(){
@@ -46,11 +44,6 @@ export class ImpressumPage {
     goToContact(){
         this.navCtrl.push(ContactPage);
     }
-
-
-
-
-
 
 
 logout() {
@@ -89,10 +82,5 @@ performLogout() {
         console.log('Dismissed toast');
     });
     logoutConf.present();
+  }
 }
-}
-
-/**
- * Logout Funktion: logout() startet einen Confirm Alert. Bei Bestätigung des Logouts wird die Funktion performLogout() ausgeführt.
- * Im Anschluß erscheint ein Toast zur Bestätigung des erfolgreichen Logouts, nachdem der Storage geleert wurde.
- */
