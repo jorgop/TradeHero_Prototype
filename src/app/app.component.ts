c2f26c70f7fd004e99f90063506cd6ee0071eef3
Katharina <Katharina.Endsuleit@gmx.de>
2018-07-10 23:34:28 +0200

    Design OCR Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,14 @@ import {HomePage} from "../pages/home/home";
 import {RegistrationPage} from "../pages/registration/registration";
 import {ContactPage} from "../pages/contact/contact";
 import {ActivityPage} from "../pages/activity/activity";
-
 import {ImpressumPage} from "../pages/impressum/impressum";
+import {OcrPage} from "../pages/ocr/ocr";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = RegistrationPage;
+  rootPage:any = OcrPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
