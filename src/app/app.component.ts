f7a39a0dbeda328a074b7838c7f4145a129201e5
Georgios <giorgo.p@web.de>
2018-07-14 14:17:22 +0200

    Fixed Image Preview on PreviewPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import {ImpressumPage} from "../pages/impressum/impressum";
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
