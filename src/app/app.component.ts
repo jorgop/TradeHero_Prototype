448c384cdf70619dfd162bca8249792064a0bb29
Johannes Brandau <mail@johannes-brandau.de>
2018-07-15 12:23:47 +0200

    Removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,6 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
-import {HomePage} from "../pages/home/home";
-import {RegistrationPage} from "../pages/registration/registration";
-import {ContactPage} from "../pages/contact/contact";
-import {ActivityPage} from "../pages/activity/activity";
-
-import {ImpressumPage} from "../pages/impressum/impressum";
-import {OcrPage} from "../pages/ocr/ocr";
-import {PreviewPage} from "../pages/preview/preview";
 
 @Component({
   templateUrl: 'app.html'
